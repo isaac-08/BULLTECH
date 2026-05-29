26a384d6c3144c117714ff6dbd059cf8718c26c2
Isaac <isaac.fonseca4@portalsesisp.org.br>
2026-05-29 08:23:05 -0300

    versão final

--- bulltech/src/pages/Admin/AdminDashboard.jsx
+++ bulltech/src/pages/Admin/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../../services/supabase';
+import './Admin.css';
 
 // Importando os ícones da sidebar
 import homeIcon from '../../assets/icons/home.png';
